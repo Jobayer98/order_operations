24fbb7eec2171c127736d0c01c574befff5f884c
Jobayer98 <joobayerrahman0220@gmail.com>
2024-05-29 15:16:56 +0600

    update vercel.json file

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-python manage.py collectstatic --noinput
-python manage.py migrate
+echo "BUILD START"
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
